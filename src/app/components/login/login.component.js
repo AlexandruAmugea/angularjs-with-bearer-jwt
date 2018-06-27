a995b52305e1dcb5c59ff88f60dec7073968132a
Alexandru Amugea <alexandru.amugea@endava.com>
2018-06-27 17:43:15 +0300

    Added login error message

--- src/app/components/login/login.component.js
+++ src/app/components/login/login.component.js
@@ -12,8 +12,8 @@ function Login(){
     }
   }
 
-  loginComponentController.$inject = ['loginService', '$state'];
-  function loginComponentController(loginService, $state) {
+  loginComponentController.$inject = ['loginService', '$state', '$mdDialog'];
+  function loginComponentController(loginService, $state, $mdDialog) {
     let vm = this;
 
     vm.user = {
@@ -21,11 +21,22 @@ function Login(){
       password: ''
     };
 
+    vm.showAlert = function() {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('Error')
+          .textContent('Wrong username or password')
+          .ok('Ok')
+      );
+    };
+
     vm.login = function(){
       loginService.login(vm.user).then(()=>{
         $state.go('home.home');
       }, (error)=> {
-        console.log(error);
+        vm.showAlert();
       })
     };
   }
